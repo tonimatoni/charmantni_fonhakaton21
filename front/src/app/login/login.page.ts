1f4d588d36039a1c33d76daf9576f7c6153c6b4f
Luka Nikolic <razvijac.softverko@gmail.com>
2021-04-10 19:52:51 +0200

    * Added municipility dropdown

--- front/src/app/login/login.page.ts
+++ front/src/app/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
+import { MunicipilityService } from '../services/municipility.service';
 import { WindowService } from '../services/window.service';
 declare const firebase;
 @Component({
@@ -12,40 +13,57 @@ declare const firebase;
 export class LoginPage implements OnInit {
   windowRef: any;
   smsCode: FormControl;
-  confirmation
-  constructor(private win: WindowService, private fsAuth: AngularFireAuth, private auth: AuthService) { }
+  loginFormGroup: FormGroup;
+  allMunicipilities:any[];
+  constructor(private win: WindowService, private fsAuth: AngularFireAuth, private auth: AuthService, private municipalityService:MunicipilityService) {
+    municipalityService.getAllMunicipilities().subscribe((allMunicipilities)=>{
+      console.log(allMunicipilities);
+      this.allMunicipilities = allMunicipilities.sort((a, b) => a.nOPS.localeCompare(b.nOPS));
+    })
+   }
 
   ngOnInit() {
-    this.smsCode = new FormControl('', [Validators.minLength(6), Validators.maxLength(6)])
+    this.loginFormGroup = new FormGroup({
+      jmbg: new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(13), Validators.pattern('^[0-9]*$')]),
+      smsCode: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(6), Validators.pattern('^[0-9]*$')]),
+      municipality: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+    })
+
   }
 
   register() {
     this.windowRef = this.win.windowRef
-    alert();
-    this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
-      'callback': (response) => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-        this.auth.registerUser('+381641377201', this.windowRef.recaptchaVerifier);
+    if (this.loginFormGroup.valid) {
+      this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+        'callback': (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          this.auth.registerUser('+381641377201', this.windowRef.recaptchaVerifier);
+        }
+      });
+      this.windowRef.recaptchaVerifier.render()
+    }else{
+      for(const c of Object.values(this.loginFormGroup.controls)){
+        c.markAllAsTouched();
       }
-    });
-    this.windowRef.recaptchaVerifier.render()
+    }
   }
 
   verifyCode() {
     const confirmationResult = this.auth.getConfirmationResult();
-    console.log(this.smsCode.valid, confirmationResult) 
-    if(this.smsCode.valid && confirmationResult){
+    console.log(this.smsCode.valid, confirmationResult)
+    if (this.smsCode.valid && confirmationResult) {
       confirmationResult.confirm(this.smsCode.value).then((result) => {
         // User signed in successfully.
         const user = result.user;
-        console.log(user); 
+        console.log(user);
         // ...
       }).catch((error) => {
-        console.log(error) 
+        console.log(error)
         // User couldn't sign in (bad verification code?)
         // ...
       });
-    }else{
+    } else {
 
     }
   }
