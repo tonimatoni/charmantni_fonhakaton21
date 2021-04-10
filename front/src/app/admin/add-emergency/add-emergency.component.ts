fe1a7ea5afe1d83b65628fad8229b55a6fd39859
tonimatoni <matts.mata@gmail.com>
2021-04-10 21:15:15 +0200

    * Emergency create form

--- front/src/app/admin/add-emergency/add-emergency.component.ts
+++ front/src/app/admin/add-emergency/add-emergency.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { EmergencyService } from 'src/app/services/emergency.service';
 
 @Component({
   selector: 'app-add-emergency',
@@ -44,19 +46,25 @@ export class AddEmergencyComponent implements OnInit {
 
   show = false;
 
-  constructor(private formBuilder: FormBuilder) {
-
+  constructor(private formBuilder: FormBuilder, private firestore: AngularFirestore, private emergencyService: EmergencyService) {
   }
 
   ngOnInit() {
   }
 
-  public submit() {
-    console.log(this.emergencyCreateForm.value)
+  public async submit() {
+    try {
+      await this.firestore
+        .collection('emergencies')
+        .add(this.emergencyCreateForm.value)
+
+      alert("Uspesno ste proglasili vanrednu sitauciju.")
+    } catch (error) {
+      alert("Nazalost je doslo do greske, pokusajte ponovo.")
+    }
   }
 
   public toggleShow() {
-    console.log("test")
     this.show = !this.show;
   }
 }
