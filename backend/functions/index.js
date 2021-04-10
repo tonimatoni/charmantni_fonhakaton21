ca9da253946831c9546dd9826c1d6f7bc68fb23c
tonimatoni <matts.mata@gmail.com>
2021-04-11 01:11:18 +0200

    * onCreate and onUpdate triggers for emergencies

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 var admin = require('firebase-admin');
 var serviceAccount = require("./serviceAcc.json");
-
+var emergencyEnums = require('./constants/emergencyEnums')
 
 
 admin.initializeApp({
@@ -35,15 +35,25 @@ exports.pushNotif = functions.https.onRequest((req, res) => {
         });
 });
 
-async function fetchByMunicipality(req, res) {
+async function fetchByMunicipality(municipalityID) {
     try {
-        const usersSnap = await db.collection('users').where('municipalityID', '==', req.body.municipalityID).get();
-        const users = usersSnap.map(doc => ({ ...doc.data(), id: doc.id }))
+        const usersSnap = await admin.firestore().collection('users').where('municipalityID', '==', municipalityID).get();
+        return usersSnap.docs.map(doc => ({ ...doc.data(), id: doc.id }))
 
     } catch (error) {
         console.log(error);
+        return [];
     }
 }
+
+async function getAdminByID(adminID) {
+    const user = await admin
+        .auth()
+        .getUser(adminID);
+
+    return user.customClaims['municipalityID'];
+}
+
 
 
 exports.createAdmin = functions.https.onRequest(async (req, res) => {
@@ -60,10 +70,68 @@ exports.createAdmin = functions.https.onRequest(async (req, res) => {
 
         await admin
             .auth()
-            .setCustomUserClaims(user.uid, { admin: true })
+            .setCustomUserClaims(user.uid, { admin: true, municipalityID: req.body.municipalityID })
         res.send(req.body)
     } catch (error) {
         res.send(error)
     }
 });
+
+exports.onCreateEmergency = functions.firestore
+    .document('emergencies/{emergencyID}')
+    .onCreate(async (snap, context) => {
+
+        const newValue = snap.data();
+        const adminID = newValue.adminID;
+        const users = await fetchByMunicipality(await getAdminByID(adminID))
+
+        const messagingTokens = users.map(u => u.messagingToken)
+
+
+        var message = {
+            data: {
+                score: '850',
+                time: '2:45'
+            },
+            tokens: messagingTokens
+        };
+
+        admin.messaging().sendMulticast(message)
+            .then((response) => {
+                console.log('Successfully sent message:', response);
+                console.log(response.successCount)
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
+    });
+
+exports.onUpdatedEmergency = functions.firestore
+    .document('emergencies/{emergencyID}')
+    .onUpdate(async (change, context) => {
+        const newValue = change.after.data();
+        const adminID = newValue.adminID;
+        const users = await fetchByMunicipality(await getAdminByID(adminID))
+
+        const messagingTokens = users.map(u => u.messagingToken)
+
+
+        var message = {
+            data: {
+                score: 'RADI L OVO LUJO BRATE?',
+                time: '2:45'
+            },
+            tokens: messagingTokens
+        };
+
+        admin.messaging().sendMulticast(message)
+            .then((response) => {
+                console.log('Successfully sent message:', response);
+                console.log(response.successCount)
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
+    });
+
 
