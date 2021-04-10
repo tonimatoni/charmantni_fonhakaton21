8700ddad518359e638aec170ad511325fbc87304
Luka Nikolic <razvijac.softverko@gmail.com>
2021-04-10 05:50:13 +0200

    * Added firebase

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,11 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+// Environment
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [ BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCH-24Qylhfu0N8pDOBs1c5N2LyTMJz4cc",
+    authDomain: "fon-hakaton21.firebaseapp.com",
+    projectId: "fon-hakaton21",
+    storageBucket: "fon-hakaton21.appspot.com",
+    messagingSenderId: "593469376966",
+    appId: "1:593469376966:web:be65649a13e7828a048ac7"
+  }
 };
 
 /*
