e6c1914c2aaedc278b544ab0d8779aa47a9b0c6a
tonimatoni <matts.mata@gmail.com>
2021-04-10 14:09:15 +0200

    *createAdmin function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,9 @@
 const functions = require("firebase-functions");
 var admin = require('firebase-admin');
 var serviceAccount = require("./serviceAcc.json");
+
+
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://fon-hakaton21.firebaseio.com'
@@ -8,7 +11,7 @@ admin.initializeApp({
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((req, res) => {
+exports.pushNotif = functions.https.onRequest((req, res) => {
     var registrationToken = 'c-RNLLbmXVBw-vyoKqUyL1:APA91bED2x5HINK312kqYj34T6YpUGGAfv29C3F2RnHo3o-l5ASb76tD3vPnvFbDyG8EsryvWuYVqAgdWIiDBlk4n0Ugp682BqgoKMDGsARXvfJKxcdYx34f8jSnlQkeZiJUd6FF4Wpm';
 
     var message = {
@@ -31,3 +34,29 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
             console.log('Error sending message:', error);
         });
 });
+
+exports.fetchByMunicipality = functions.https.onRequest(async (req, res) => {
+    
+});
+
+exports.createAdmin = functions.https.onRequest(async (req, res) => {
+    try {
+        const user = await admin
+            .auth()
+            .createUser({
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                password: req.body.password,
+                displayName: req.body.name,
+                disabled: false,
+            })
+
+        await admin
+            .auth()
+            .setCustomUserClaims(user.uid, { admin: true })
+        res.send(req.body)
+    } catch (error) {
+        res.send(error)
+    }
+});
+
