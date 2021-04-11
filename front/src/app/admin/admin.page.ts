6a6f8f54062d0c4f262cf17f72041f5c2e3fae2b
Luka Nikolic <razvijac.softverko@gmail.com>
2021-04-11 10:07:06 +0200

    * Added logout button

--- front/src/app/admin/admin.page.ts
+++ front/src/app/admin/admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-admin',
@@ -8,13 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class AdminPage implements OnInit {
 
   
-  constructor() {
+  constructor(private fsAuth:AngularFireAuth) {
 
   }
 
   ngOnInit() {
   }
 
-  
+  logout(){
+    this.fsAuth.signOut();
+  }
 
 }
