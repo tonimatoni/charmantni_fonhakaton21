fdd66fb19c53aa9fc85ee3bebfb04caa71877baa
Luka Nikolic <razvijac.softverko@gmail.com>
2021-04-11 08:27:28 +0200

    * Get current user data

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { userInfo } from 'node:os';
 declare const window;
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   confirmationResult:any;
-  constructor(private fsAuth: AngularFireAuth) { }
+  constructor(private fsAuth: AngularFireAuth, private firestore:AngularFirestore) { }
 
   async registerUser(phoneNumber, appVerifier){
     this.confirmationResult = await this.fsAuth.signInWithPhoneNumber(phoneNumber, appVerifier);
@@ -15,5 +17,14 @@ export class AuthService {
 
   getConfirmationResult(){
     return this.confirmationResult;
+  }
+
+  async getCurrentUser(){
+    const user = await this.fsAuth.currentUser;
+    const userSnap =  await this.firestore.doc(`users/${user.uid}`).get().toPromise();
+    return {
+      ...userSnap.data() as any,
+      id:userSnap.id
+    }
   }
 }
